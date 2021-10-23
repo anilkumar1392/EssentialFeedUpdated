72d87cdbbb8e7d14e247b59344c509454dff18f2
Anil kumar <anil1.kumar@africa.airtel.com>
2021-10-23 11:06:37 +0530

    Give a url instead of hard coding it in RemoteFeedlaoder

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,7 +7,16 @@
 
 import XCTest
 
+
 class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
     func load() {
         /*
          In production code we do not want to set aa proprety but we want to call a method to get data from Url.
@@ -16,28 +25,54 @@ class RemoteFeedLoader {
          2. We don't want requestedUrl as this is just for testing not in production so Let's move this to a different class by making HttpClient a var.
          3. Move the test logic to a new class HttpClientSpy subclass of HTTPClient
          */
-        // HTTPClient.shared.requestedURL = URL(string: "https.goolge.com")
-        HTTPClient.shared.get(from: URL(string: "https.goolge.com"))
+         
+        //1.  HTTPClient.shared.requestedURL = URL(string: "https.goolge.com")
+        //2. HTTPClient.shared.get(from: URL(string: "https.goolge.com"))
+        
+        client.get(from: url)
+        
+        /*
+         Above we are mixing the responsiblity
+         1. Responsibility of Invoking the get method.
+         2. Responsibility of locating this shared object.
+         
+         /*
+          By using Singleton I know how to locate the instance I'm using and I dont need to konw.
+          So by using Dependency Injection we can remove this responsibilty and we ahve more control.
+          */
+         
+        */
     }
 }
 
+/*
 class HTTPClient {
-    static var shared = HTTPClient()
+    /*
+     As we are usign Dependecy Injection we do not need shared instance.
+     */
+
+    // static var shared = HTTPClient()
+    
     /*
      Not a singleton anymore private init can be removed
      */
     //private init() {}
     func get(from url: URL?) {}
-}
+} */
 
+protocol HTTPClient {
+    func get(from url: URL?)
+}
 
 /*
  Test specific method.
+ Their is nothing wrong with subclassing but their are better approaches. we can use composition rather than Inheritance.
  */
+
 class HttpClientSpy: HTTPClient {
     var requestedURL: URL?
     
-    override func get(from url: URL?) {
+    func get(from url: URL?) {
         guard let url = url else {return}
         requestedURL = url
     }
@@ -50,9 +85,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         /*
          Test logic is now test type and we do nto have singleton any more
          */
+        let url = URL(string: "https.goolge.com")!
         let client = HttpClientSpy()  //HTTPClient.shared
-        HTTPClient.shared = client
-        let _ = RemoteFeedLoader()
+        //HTTPClient.shared = client
+        let _ = RemoteFeedLoader(url: url, client: client)
         
         // Act
         
@@ -65,12 +101,14 @@ class RemoteFeedLoaderTests: XCTestCase {
         /*
          Test logic is now test type and we do nto have singleton any more
          */
+        let url = URL(string: "https.goolge.com")!
         let client = HttpClientSpy()  //HTTPClient.shared
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        //HTTPClient.shared = client
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
