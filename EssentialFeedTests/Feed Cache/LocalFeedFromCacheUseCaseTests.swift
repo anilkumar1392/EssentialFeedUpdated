d938d1aeb0cc48cfccef60744931eaa8647cfcbc
Anil kumar <anil1.kumar@africa.airtel.com>
2022-05-21 17:28:46 +0530

    Load command delivers no mages on more than seven days old cache

--- EssentialFeedTests/Feed Cache/LocalFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LocalFeedFromCacheUseCaseTests.swift
@@ -117,6 +117,17 @@ class LocalFeedFromCacheUseCaseTest: XCTestCase {
         }
     }
     
+    func test_load_deliversNoImagesOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let sevenDaysTimeStamp = fixedCurrentDate.adding(days: -7).adding(days: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success([])) {
+            store.completeRetrival(with: feed.local, timestamp: sevenDaysTimeStamp)
+        }
+    }
+    
     // MARK: - Heleprs
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
