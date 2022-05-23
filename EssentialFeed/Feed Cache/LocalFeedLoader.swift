fcc4f517aa45d6439e865ac8063e9ff607be3d1c
Anil kumar <anil1.kumar@africa.airtel.com>
2022-05-23 13:55:26 +0530

    Understanding application specific and application agnostic business logic

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
+/*
+ Presistance Module:
+ Separating App-specific, App-agnostic & Framework logic, Entities vs. Value Objects, Establishing Single Sources of Truth, and Designing Side-effect-free (Deterministic) Domain Models with Functional Core, Imperative Shell Principles
+ 
+ Independent modules with clear inetfaces.
+ 
+ ///We separate application sepcific details from bussiness rules.
+
+ we separate application specific details from bussines rules.
+ Contollers are not business models, they communicate with business model to solve application specific business rules.
+ 
+ Seperating bussiness model, controller and framework is a key to achieve modularity.
+ To achieve freedom, to achieve freedom and testibility.
+ 
+ You dictate your architecture not the framework will dictate your architecture.
+ You plugin the framework to solve infraStructure details.
+ 
+ /// Searating Application specific logic from concrete framework details. (like coredata, realm)
+ So feedStore protocol protects our controller from depending on concrete store implemenation like (Coradata, realm, file system)
+ */
+
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
