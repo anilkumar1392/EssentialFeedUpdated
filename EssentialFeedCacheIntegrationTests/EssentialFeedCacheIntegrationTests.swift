7bb277842f065c2cf581e741c85a7eecb2b1ac3f
Anil kumar <anilpal1392@gmail.com>
2022-06-02 07:12:07 +0530

    'LocalFeedLoader' in integration with the 'CodeDataFeedStore' delivers items saved on separate instance, proving we correctly persists the data in the disk

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -36,6 +36,36 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1)
     }
     
+    func test_load_deliversInstanceSavedOnSeparateInstances() {
+        let sutToPerforSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "wait for save operation to complete")
+        sutToPerforSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        
+        wait(for: [saveExp], timeout: 1.0)
+
+        let loadExp = expectation(description: "wait for load operation to complete")
+        sutToPerformLoad.load { result in
+            switch result {
+            case let .success(feeds):
+                XCTAssertEqual(feeds, feed)
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed results, got \(error) instead.")
+            }
+            
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 1.0)
+        
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
