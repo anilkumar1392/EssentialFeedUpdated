43264ace5acf3316a2f5df292e75674e187b9e0c
Anil kumar <anilpal1392@gmail.com>
2022-06-13 15:33:01 +0530

    Move 'MainQueueDecorator' to a separate file

--- EssentialFeediOS/Composer/FeedUIComposer.swift
+++ EssentialFeediOS/Composer/FeedUIComposer.swift
@@ -85,48 +85,6 @@ public final class FeedUIComposer {
     }
 }
 
-/*
- 
-   1.  Add behaviour to an instance wthout while keeping the same interface
-   Following Open and close principle.
-   Adding behaviour without changing the insatacne.
- 
-   So the ViewModel does not know about threading, Controller does not know about threading.
- 
- */
-
-final class MainQueueDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        
-        completion()
-    }
-}
-   
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
-extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataTaskLoader {
-        decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
 private extension FeedViewController {
     static func makeWith() -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
