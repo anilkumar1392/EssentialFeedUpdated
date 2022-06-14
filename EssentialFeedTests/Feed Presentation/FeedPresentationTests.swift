46f109e4bedfc477b8f1db8c9a4afc8fc17d6bb8
Anil kumar <anilpal1392@gmail.com>
2022-06-14 17:41:51 +0530

    Extract sut creation in to a test factory method

--- EssentialFeedTests/Feed Presentation/FeedPresentationTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresentationTests.swift
@@ -17,11 +17,19 @@ class FeedPresenter {
 class FeedPresenationTest: XCTestCase {
     
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
-        
-        _ = FeedPresenter(view: view)
-        
+        let (sut, view) = makeSUT()
+ 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(view, file: file, line: line)
+        return (sut, view)
     }
 }
 
