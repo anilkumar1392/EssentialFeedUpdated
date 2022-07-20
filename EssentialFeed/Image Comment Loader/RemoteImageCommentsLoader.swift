481888df4eea169467ae4a37a00af395ad01b6cf
Anil kumar <anilpal1392@gmail.com>
2022-07-20 12:46:54 +0530

    Replace RemoteImageCommentLoader with generic RemoteLoader to remove duplication

--- EssentialFeed/Image Comment Loader/RemoteImageCommentsLoader.swift
+++ EssentialFeed/Image Comment Loader/RemoteImageCommentsLoader.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
+
+public extension RemoteImageCommentsLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: ImageCommentsMapper.map)
+    }
+}
+
+/*
 public final class RemoteImageCommentsLoader {
     private let url: URL
     private let client: HTTPClient
@@ -44,3 +53,4 @@ public final class RemoteImageCommentsLoader {
         }
     }
 }
+ */
--- EssentialFeedTests/Image Comment Api/LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Image Comment Api/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -11,82 +11,6 @@ import EssentialFeed
 
 class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
     
-    func test_init_doesNotRequestDataFromURL() {
-        // Arrange
-        /*
-         Test logic is now test type and we do nto have singleton any more
-         */
-        let client = HttpClientSpy()  //HTTPClient.shared
-        //HTTPClient.shared = client
-        let _ = makeSUT()
-        
-        // Act
-        // Assert
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-
-    
-    func test_load_requestsDataFromUrL(){
-        /*
-         Test logic is now test type and we do not have singleton any more
-         */
-        let url = URL(string: "https.goolge.com")!
-        // let client = HttpClientSpy()  //HTTPClient.shared
-        //HTTPClient.shared = client
-        let (sut,client) = makeSUT(url: url)
-        sut.load { _ in }
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataTwice() {
-        let url = URL(string: "https.goolge.com")!
-        let (sut,client) = makeSUT(url: url)
-        sut.load { _ in }
-        sut.load { _ in }
-        XCTAssertEqual(client.requestedURLs, [url,url])
-    }
-    
-    // No Connectivity
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        // client.error = NSError(domain: "test", code: 0) Stub
-        
-        //var capturedError: RemoteImageCommentsLoader.Error
-
-        /*
-        var capturedError = [RemoteImageCommentsLoader.Result]()
-        sut.load { capturedError.append($0) }*/
-        
-        let exp = expectation(description: "wait for expectation")
-        
-        let expectedResult: RemoteImageCommentsLoader.Result = .failure(RemoteImageCommentsLoader.Error.connectivity)
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedItems), .success(expectedItems)):
-                XCTAssertEqual(receivedItems, expectedItems)
-                
-            case let (.failure(receivedFailure as RemoteImageCommentsLoader.Error), .failure(expectedFailure as RemoteImageCommentsLoader.Error)):
-                XCTAssertEqual(receivedFailure, expectedFailure)
-                
-            default:
-                XCTFail("Expected result is \(expectedResult) received reuslt is \(receivedResult) instead")
-            }
-            
-            exp.fulfill()
-            
-        }
-        
-        
-        //Stub replaced with capturing
-        let clientError = NSError(domain: "test", code: 0)
-        // client.completions[0](clientError)
-        
-        client.complete(with: clientError)
-        
-        wait(for: [exp], timeout: 1.0)
-        // XCTAssertEqual(capturedError, [.failure(.connectivity)])
-    }
-    
     // Invalid Data
     func test_load_deliversErrorOnNon2xxHttpsResponse() {
         let (sut, client) = makeSUT()
@@ -152,20 +76,6 @@ class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
                 client.complete(withStatusCode: code, data: json, at: index)
             }
         }
-    }
-    
-    func test_load_doesNotDeliverResultAfterSutInstanceHasbeenDeallocated() {
-        let url = URL(string: "https://anyurl.com")!
-        let client = HttpClientSpy()
-        var sut: RemoteImageCommentsLoader? = RemoteImageCommentsLoader(url: url, client: client)
-
-        var capturedResults = [RemoteImageCommentsLoader.Result]()
-        sut?.load { capturedResults.append($0) }
-        
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemJson([]))
-        
-        XCTAssertTrue(capturedResults.isEmpty)
     }
     
     //MARK: Helpers
